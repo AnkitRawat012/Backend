7e5229173bcf2d50836525ec9f99ae6bb92d940c
Ankit Rawat <ankitrawat@Ankits-MacBook-Air.local>
2026-02-07 23:32:05 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import dotenv from "dotenv";
+
 import connectDB from "./db/db.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 
-
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`🕸️ Server is running at port ${process.env.PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db Connection Failed !!! ", error);
+  });
 /*
 (async ()=>{
     try {
